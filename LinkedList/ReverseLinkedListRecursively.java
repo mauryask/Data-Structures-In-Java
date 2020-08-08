41d72cc5dcd6f45adfe9f3a17d2d49b571ea8d21
BlackHatos <pnssoftwares7@gmail.com>
2020-08-09 04:50:09 +0530

    reverse a linked list recursivly

---
+++ LinkedList/ReverseLinkedListRecursively.java
@@ -0,0 +1,75 @@
+//This prgram demonstrates how to revrse a linked list
+//using recursive approach 
+
+class Node 
+{
+	int value;
+	Node next;
+	Node(int value)
+	{
+		this.value = value;
+		next = null;		
+	}
+}
+
+class ReverseLinkedListRecursively
+{
+	static Node head = null;
+	static void insertNode(int value)
+	{
+		Node ptr, temp;
+		temp = new Node(value);
+		if(head== null)
+			head = temp;
+		else
+		{
+			ptr = head;
+			while(ptr.next != null)
+				ptr = ptr.next;
+			ptr.next = temp;
+		}
+	}
+	
+	static void displayList()
+	{
+		Node ptr = head;
+		while(ptr != null)
+		{
+			System.out.print(ptr.value+" ");
+			ptr = ptr.next;
+		}
+	}
+	
+	static Node recursiveReverse(Node head)
+	{
+		if(head == null)
+			return null;
+		else if(head.next == null)
+			return head;
+		else
+		{
+			Node nextNode = head.next;
+			head.next = null;
+			Node rest = recursiveReverse(nextNode);
+			nextNode.next = head;
+			return rest;
+		}
+	}
+	
+	public static void main(String [] args)
+	{
+		insertNode(45);
+		insertNode(78);
+		insertNode(89);
+		insertNode(23);
+		insertNode(20);
+		insertNode(14);
+		insertNode(69);
+		
+		displayList();
+		
+		head = recursiveReverse(head);
+		System.out.println();
+		displayList();
+	}		
+}
