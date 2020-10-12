c52fa1323d9008506a1b9d86df2122143c8506d4
BlackHatos <pnssoftwares7@gmail.com>
2020-10-12 08:10:55 +0530

    Updated

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,3 +1,4 @@
+//here you need to sort the element any sorting technique
 import static java.lang.System.*;
 
 public class BinarySearch 
@@ -5,15 +6,15 @@ public class BinarySearch
 		public static void main(String [] args)
 		{
 
-			int A[] = new int []{1,2,3,4,5,6,7,8,9}; //here you need to sort the element any sorting technique
-			int target = 7;
-		        int index=BinarySearch.binary_search(A, target);
+			int A[] = new int []{1,2,3,4,5,6,7,8,9}; 
+			int target = 12;
+		    int index=BinarySearch.binary_search(A, target);
 		    out.println("The element is found at index: "+index);
 		}
 
 		static int binary_search(int A[], int target)
 		{
-
+			boolean flag = false;
 			int m_index, l_index, r_index;
 			m_index=0;
 			l_index = 0;
@@ -22,13 +23,16 @@ public class BinarySearch
 			{
 				m_index = (l_index+r_index)/2;
 				if(target == A[m_index])
-					return m_index;
+				{
+					flag = true;
+					break;
+				}
 				else if(target > A[m_index])
 					l_index = m_index+1;
 				else 
 					r_index = m_index-1;	
 			}
 
-			return m_index;
+			return flag ? m_index : -1;
 		}
 }
