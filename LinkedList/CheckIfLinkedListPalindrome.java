a56ad12c4a0a6c61743d12973795c41d99ae92c9
BlackHatos <pnssoftwares7@gmail.com>
2020-10-20 17:17:38 +0530

    Updated

--- LinkedList/CheckIfLinkedListPalindrome.java
+++ LinkedList/CheckIfLinkedListPalindrome.java
@@ -1,6 +1,8 @@
 // checking if list is palindrome or not
 // Time complexity : O(n)
+// Space Complexity : O(1)
 
+import static java.lang.System.*;
 class Node 
 {
 	int data;
@@ -37,46 +39,53 @@ public class CheckIfLinkedListPalindrome
 	public static void main(String [] args)
 	{
 		Node head = null;
-		
 	    head = insertNode(78, head);
 		insertNode(56, head);
 		insertNode(60, head);
 		insertNode(50, head);
-		//insertNode(50, head);
+		insertNode(50, head);
 		insertNode(60, head);   
 		insertNode(56, head);   
 		Node end = insertNode(78, head); 
        
-	   
-	     if(new Solver().findMidNode(head))
-              System.out.println("Plaindrome");
-           else
-			System.out.println("Not Paindrome");
+	    out.println(checkPalin(head));
 	}
-}
-
-class Solver
-{
 	
-  public boolean findMidNode(Node head)
+   static boolean checkPalin(Node head)
    {
+	   if(head == null || head.next == null)
+		   return true;
+	   
 	   Node p1 = head;
 	   Node p2 = head;
-	   Node newHead = null;
-	   Node temp = null;
+	   
+	   Node revHead = null;
+
 	   while(p2!= null && p2.next != null)
 	   {
-		       temp = p1;
 			   p1 = p1.next;
 			   p2 = p2.next.next;
 	   }
 	   
-	   Node head2 = reverseList(p1);
-	   return isPalin(head, head2);
+	   revHead = reverse(p1);
+	   boolean flag = true;
+	   
+	   while(head != p1)
+	   {
+		   if(head.data != revHead.data)
+		   {
+			   flag = false;
+			   break;
+		   }
+		   head = head.next;
+		   revHead = revHead.next;
+	   }
+	   
+	   return flag;
     }
 
 
-  public Node reverseList(Node head)
+      static Node reverse(Node head)
 	  {
 		 Node ptr = head; 
 		 Node prevPtr = null;
@@ -92,18 +101,5 @@ class Solver
 		 
 		 return prevPtr;
 	  }
-	
-
-    static boolean isPalin(Node head, Node head2)
-	{
-		while(head2 != null )
-		{
-			if(head.data != head2.data)
-				return false;
-			head = head.next;
-			head2 = head2.next;
-		}
-		return true;
-	}	
-}	
+}
 
