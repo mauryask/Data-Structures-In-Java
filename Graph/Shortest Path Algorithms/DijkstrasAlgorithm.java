d98fc25a0a27470f596b75a4ece4bd545d22b07b
shubhammaurya <pnssoftwares7@gmail.com>
2021-08-01 10:25:04 +0530

    Updated

--- Graph/Shortest Path Algorithms/DijkstrasAlgorithm.java
+++ Graph/Shortest Path Algorithms/DijkstrasAlgorithm.java
@@ -1,124 +1,142 @@
 // Time Complexty : O(V^2)
 // It can be reduced to : O(E*logV) using min heap
 // It works for both dircetd and undirected graph
+// Single source shortest path algorithm
 
+import static java.lang.System.*;
 import java.util.*;
-class Graph
+
+public class DijkstrasAlgorithm
 {
-	class Edge
-	{
-		int v;
-		int w;
-		
+   class Edge 
+   {
+	   int v; 
+	   int w;
+	   
 	   Edge(int v, int w)
 	   {
 		   this.v = v;
 		   this.w = w;
-	   }	   
-	}
-	
-	int v_num;
-    List<Edge> G[];	
-	
-	Graph(int v_num)
-	{
-		this.v_num = v_num;
-		G = new LinkedList[v_num];
-		for(int i=0; i<v_num; i++)
-		  G[i] = new LinkedList<Edge>();
-	}
-	
-	void addEdge(int u, int v, int w)
-	{
-		G[u].add(0, new Edge(v,w));
-		G[v].add(0, new Edge(u,w));
-	}
-		
-	int parent[];	
-		
-	void dijkstra(int src)
-	{
-		boolean visited[] = new boolean[v_num];
-		int dist[] = new int[v_num];
-		parent = new int[v_num];
-		
-		for(int i=0; i<v_num; i++)
-		{
-			dist[i] = Integer.MAX_VALUE;
-			parent[i] = -1;
-		}
-		
-		dist[src] = 0;
-		
-	    for(int i=1; i<=v_num; i++) //that means run |v| - times
-		{
-			int u = minDist(dist, visited);
-						
-			for(Edge e : G[u])
+	   }
+	   
+	   @Override
+	   public String toString()
+	   {
+		   return "("+ v + "," + w + ")";
+	   }
+   }   
+   
+   List<Edge> adj[];
+   DijkstrasAlgorithm(int n)
+   {
+	   adj = new LinkedList[n];
+	   for(int i=0; i<n; i++)
+		   adj[i] = new LinkedList<Edge>();
+   }
+   
+   @Override
+   public String toString()
+   {
+	   String result = "";
+	   for(int i=0; i<adj.length; i++)
+		   result += i +" => " + adj[i]+"\n";
+	   return result;
+   }
+   
+   void addEdge(int u, int v, int w)
+   {
+	   adj[u].add(new Edge(v, w));
+	   adj[v].add(new Edge(u, w));
+   }
+   
+   void dijkstra(int src, int n, int dest)
+   {
+	   boolean visited[] = new boolean[n];
+	   int dist[] = new int[n];
+	   int parent[] = new int[n];
+	   
+	   for(int i=0; i<n; i++)
+	   {
+		   dist[i] = Integer.MAX_VALUE;
+		   parent[i] = -1;
+	   }
+	   
+	   dist[src] = 0;
+	   
+	   for(int i=0; i<n; i++)
+	   {
+		    int u = findMin(dist, visited, n);		
+			visited[u] = true;
+			for(Edge e : adj[u])
 			{
 				if(!visited[e.v] && (dist[u] + e.w) < dist[e.v])
 				{
 					dist[e.v] = dist[u] + e.w;
 					parent[e.v] = u;
 				}
-			}	
-			visited[u] = true;
-		}
-	
-		for(int i=0; i<v_num; i++)
-			Sylstem.out.println(src+" ==> "+i+" = "+dist[i]);
-	}
-       
-	void printPath(int dest)
-	{
-		int crawl =  dest;
-		List<Integer> path = new LinkedList<>();
-		path.add(crawl);
-		
-		while(parent[crawl] != -1)
-		{
-			path.add(0,parent[crawl]);
-			crawl = parent[crawl];
-		}
-		
-		System.out.println("The path: ");
-		for(int x: path)
-			System.out.print(x+" ");
-	}	
-	   
-    int minDist(int dist[], boolean visited[])
-	{
-		int min = Integer.MAX_VALUE;
-		int min_index = 0;
-		for(int i=0; i<v_num; i++)
-		{
-			if(!visited[i] && dist[i] <= min)
-			{
-				min = dist[i];
-				min_index = i;
 			}
-		}
-		return min_index;
-	}		
-}
-
-public class DijkstrasAlgorithm
-{
-	public static void main(String [] args)
-	{
-		Graph g = new Graph(6);
-		g.addEdge(0,1,4);
-		g.addEdge(0,3,10);
-		g.addEdge(0,2,5);
-		g.addEdge(1,2,2);
-		g.addEdge(1,4,3);
-		g.addEdge(3,2,7);
-		g.addEdge(2,4,1);
-		g.addEdge(2,5,6);
-		g.addEdge(4,5,11);
-		g.addEdge(3,5,3);
-		
-		g.dijkstra(0);
-		g.printPath(5);
-	}
+	   }
+     
+      for(int i=0; i<n; i++)
+		out.println(src+ " ==> " + i +"  =  "+ dist[i]);
+      printPath(parent, n, dest);	
+   }
+   
+   void printPath(int parent[], int n, int dest)
+   {
+	   Stack<Integer> path = new Stack<>();
+	   int temp = parent[dest];
+	   path.push(dest);
+	   
+	   while(temp != -1)
+	   {
+		   path.push(temp);
+		   temp =  parent[temp];
+	   }
+	   out.println();
+	   for(;!path.isEmpty();)
+		   out.print(path.pop()+" ");
+	   out.println();
+   }
+   
+   int findMin(int dist[], boolean visited[], int n)
+   {
+	   int min = Integer.MAX_VALUE;
+	   int index = 0;
+	   
+	   for(int i=0; i<n ; i++)
+	   {
+		   if(!visited[i] && min > dist[i])
+		   {
+			   min = dist[i];
+			   index = i;
+		   }
+	   }
+	   
+	   return index;
+   }
+   
+   public static void main(String [] args)
+   {
+	   int n = 9;
+	   DijkstrasAlgorithm g = new DijkstrasAlgorithm(n);
+	   g.addEdge(0,1,4);
+	   g.addEdge(0,4,8);
+	   g.addEdge(1,4,11);
+	   g.addEdge(4,5,1);
+	   g.addEdge(4,8,7);
+	   g.addEdge(8,2,2);
+	   g.addEdge(1,2,8);
+	   g.addEdge(2,3,7);
+	   g.addEdge(2,6,4);
+	   g.addEdge(5,6,2);
+	   g.addEdge(3,6,14);
+	   g.addEdge(3,7,9);
+	   g.addEdge(6,7,10);
+	   g.addEdge(8,5,6);
+	   
+	   out.println(g);
+	   
+	   g.dijkstra(0, n, 7);
+   }
 }
--- Graph/Shortest Path Algorithms/ShortestPathInUnweightedGraph.java
+++ Graph/Shortest Path Algorithms/ShortestPathInUnweightedGraph.java
@@ -1,150 +1,109 @@
-import java.util.LinkedList;
-import java.util.List;
-import static java.lang.System.out;
-import static java.lang.System.in;
+/**
+* Time Complexity : O(E+V)
+* Space Complexity : O(V)
+* Works for both directed and 
+* undirected 'connected' graph
+*/
 
-class Graph 
-{
-	int v_num;
-	List<Integer> G[];
-	
-	Graph(int v_num)
-	{
-		this.v_num = v_num;
-		G = new LinkedList[v_num];
-		for(int i=0; i<G.length; i++)
-			G[i] = new LinkedList<Integer>();
-	}
- 
-  void addEdge(int u, int v)
-  {
-	  //directed graph
-	  G[u].add(0,v);
-	  //G[v].add(0,u);
-  }
-
-	public String toString()
-	{
-		String result = "";
-		for(int i=0; i<v_num; i++)
-			result += i+" ==>"+G[i]+"\n";
-		return result;
-	}
-		
-	boolean isConnected(int u, int v)
-	{
-		for(Integer i : G[u])
-		if(i == v)
-			 return true;
-		return false;
-	}
-
-
-   void findshortestPath(int s, int d)
-   {
-	   int distance[] = new int[v_num];
-	   int parent[] = new int[v_num];
-	  /* if(!bfSearch(s,d,distance,parent))
-	   {
-	      	System.out.println("They are not connected...");
-			return;
-	   }	
-	  
-	  System.out.println("Distance: "+distance[d]);
-	 
-	  LinkedList<Integer> path = new LinkedList<>();
-	  int crawl = d;
-	  path.add(crawl);
-	  
-	  while(parent[crawl] != -1)
-	  {
-		crawl = parent[crawl];
-		path.add(0, crawl);
-	  }
-	  
-	  for(int i = path.size()-1 ; i >= 0; i--)
-		  System.out.print(path.get(i)+" ");*/
-	  
-	  //or we can use a recursive method to print shorest path
-	    bfSearch(s,d,distance,parent);
-		printPathRecursively(s, d, parent);
-	  
-	  }
-	  
-	  void printPathRecursively(int s, int d, int  parent[]) //destination vertex
-	  {
-		    if(d == s)
-			 System.out.print(s+" ");
-			else if(parent[d] == -1)
-			{
-				System.out.println("They are not connected..");	
-				return;
-			}
-			else
-			{
-				printPathRecursively(s,parent[d],parent);
-		        System.out.print(d+" ");
-			}
-	 }
-	
-	boolean bfSearch(int s, int d, int distance[],int parent[])
-	{
-	   boolean visited[] = new boolean[v_num];
-	   Queue<Integer> queue = new ArrayDeque<>();
-	   
-	   for(int i=0; i<v_num; i++)
-	   {
-		   visited[i] = false;
-		   distance[i] = Integer.MAX_VALUE;
-		   parent[i] = -1;
-	   }
-	   
-	   visited[s] = true;
-	   distance[s] = 0;
-
-	   queue.add(s);
-	   
-	   while(!queue.isEmpty())
-	   {
-		   int u = queue.poll();
-		   for(int v : G[u])
-		   {
-			   if(!visited[v])
-			   {
-				   visited[v] = true;
-				   distance[v] = distance[u] + 1;
-				   queue.add(v);
-				   parent[v] = u;
-				   if(v == d)
-					  return true;
-			   }
-		   }
-	   }
-	   
-       return false;
-	}
-}
+import static java.lang.System.*;
+import java.util.*;
 
 public class ShortestPathInUnweightedGraph
 {
+    List<Integer> adj[];
+	public Graph(int n)
+	{
+		adj = new LinkedList[n];
+		for(int i=0; i<n; i++)
+			adj[i] = new LinkedList<Integer>();
+	}
+	
+	@Override
+	public String toString()
+	{
+		String result = "";
+		for(int i=0; i<adj.length; i++)
+			result += i + " => " + adj[i] +"\n";
+		return result;
+	}
+	
+	void addEdge(int u, int v)
+	{
+		adj[u].add(v);
+		adj[v].add(u);
+	}
+	
+	
+	void shortestPath(int n, int src, int dest)
+	{
+	  int parent[] = new int[n];
+	  int distance[] = new int[n];
+	  
+	  for(int i=0; i<n; i++)
+	  {
+		  parent[i] = -1;
+		  distance[i] = Integer.MAX_VALUE;
+	  }
+	  
+	  shortestPathUtil(n, src, dest, distance, parent);
+	  
+       Stack<Integer> stack = new Stack<>();
+       stack.push(dest);
+       int temp = parent[dest];
+	   
+       while(temp != -1)
+	   {
+		   stack.push(temp);
+		   temp = parent[temp];
+	   }
+      	
+       for(;!stack.isEmpty();)
+		out.print(stack.pop()+" ");		   
+	}
+	
+	
+	void shortestPathUtil(int n, int src, int dest, int distance[], int parent[])
+	{
+		List<Integer> q = new LinkedList<>();
+		boolean visited[] = new boolean[n];
+		
+		q.add(src);
+		visited[src] = true;
+		distance[src] = 0;
+		
+		while(!q.isEmpty())
+		{
+			int u = q.remove(0);
+			
+			for(int v : adj[u])
+			{
+				if(!visited[v])
+				{
+					visited[v] = true;
+					q.add(v);
+					distance[v] = distance[u] + 1;
+					parent[v] = u;
+					
+					if(v == dest)
+						return;
+				}
+			}
+		}
+	}
+	
 	public static void main(String [] args)
 	{
-		Graph g = new Graph(7);
-		
+		int n = 7;
+		Graph g = new Graph(n);
 		g.addEdge(0,1);
-		g.addEdge(2,0);
-		g.addEdge(6,2);
-		g.addEdge(3,6);
-		g.addEdge(2,1);
-		g.addEdge(1,4);
-		g.addEdge(6,5);
-		g.addEdge(5,3);
-		g.addEdge(5,4);
-		//g.addEdge(1,3);
+		g.addEdge(0,4);
+		g.addEdge(4,5);
+		g.addEdge(4,3);
+		g.addEdge(1,2);
+		g.addEdge(2,3);
+		g.addEdge(5,6);
+		out.println(g);
 		
-		System.out.println(g);
-		System.out.println("=================\n");	
-		g.findshortestPath(3,4);
-	
+		g.shortestPath(n, 0, 2);		
 	}
 }
