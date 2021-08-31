fb86dff8202a7446e51977ff460aa59aae1e2dab
shubhammaurya <pnssoftwares7@gmail.com>
2021-08-31 10:15:16 +0530

    Updated

--- Stack/MaxAreaHistogram.java
+++ Stack/MaxAreaHistogram.java
@@ -1,16 +1,35 @@
+/*
+* Time complexity  : O(n)
+* Space complexity : O(n)
+**************************
+* The idea id to find the next smaller 
+* bar to the left and next smaller bar to the rightSmaller
+* of the current bar (rightSmaller - leftSmaler - 1)
+* is going to be the width of the rectangle formed
+* by the current bar
+******
+* area of rectangle formed by each bar
+* height of bar *  width of rectangle formed by this
+*/
+
 import static java.lang.System.*;
 import java.util.*;
 public class MaxAreaHistogram
 {
 	static int findMaxArea(int height[], int n)
 	{
+		// find next smaller bar to the left of each
 		int leftSmaller[] = leftSmaller(height, n);
+		// find right smaller bar to the right of each
 		int rightSmaller[] = rightSmaller(height, n);
+		
 		int maxArea = Integer.MIN_VALUE;		
 		
 		for(int i=0; i<n; i++)
 		{
+			// find width of rectangle
 			int width = rightSmaller[i] - leftSmaller[i] -1;
+			// find max area
 			maxArea = Math.max(maxArea, height[i] * width);
 		}	
 		
@@ -26,9 +45,13 @@ public class MaxAreaHistogram
 		{
 			while(!stack.isEmpty() && height[stack.peek()] >= height[i])
 				stack.pop();
+			// if stack is empty means
+			// no such bar to the right of the bar
+			// which is smaller than this
+			// assume that it will be present at index: n (hypothetically)
 			if(stack.isEmpty())
 				temp[i] = n;
-			else
+			else 
 				temp[i] = stack.peek();
 			stack.push(i);
 		}
@@ -45,6 +68,10 @@ public class MaxAreaHistogram
 		{
 			while(!stack.isEmpty() && height[stack.peek()] >= height[i])
 				stack.pop();
+			// if stack is empty means
+			// no such bar to the left of the bar
+			// which is smaller than this
+			// assume that it will be present at index: -1 (hypothetically)
 			if(stack.isEmpty())
 				temp[i] = -1;
 			else
