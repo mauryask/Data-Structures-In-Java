fd75e618b277656be5f5fe6ddfbae0e0d57da69c
shubhammaurya <pnssoftwares7@gmail.com>
2021-09-16 14:59:57 +0530

    Updated

--- Arrays/CountAllTheSubArraysWithSumK.java
+++ Arrays/CountAllTheSubArraysWithSumK.java
@@ -20,13 +20,17 @@ public class CountAllTheSubArraysWithSumK
 		int n = A.length;
 		Map<Integer, Integer> map = new HashMap<>();
 		map.put(0,1);
+		
 		int ans = 0;
 		int sum = 0;
+		
 		for(int i=0;i<n; i++)
 		{
 			sum += A[i]; 
+			
 			if(map.containsKey(sum-target))
 			  ans += map.get(sum-target);
+		  
 		    map.put(sum, map.getOrDefault(sum,0)+1);
 		}
 		return ans;
--- Arrays/CountSubarraysWithSumDivisibleByK.java
+++ Arrays/CountSubarraysWithSumDivisibleByK.java
@@ -1,72 +1,29 @@
-// Question Number : #1
-
-// counting all the sub arrays 
-// with cumulative sum divisible by k
-// Time Complexity  : O(n+k)
-// Space Complexity : O(k)
-
-// see the explanation in the notes
+/*
+* T(n) : O(n) 
+* S(n) : O(n)
+*/
 
 import static java.lang.System.*;
 import java.util.*;
 
 public class CountSubarraysWithSumDivisibleByK
 {
-	public static void main(String [] args)
-	{   int A[] = {4, 5, 0, -12, -23, 1 };
-		int k = 5;
-		solve(A, k);
-		bruteForce(A,k);
-	}
-	
-	static void solve(int A[], int k)
+	/*
+	* T(n) : O(n*n)
+	* S(n) : O(1)
+	*/
+	static void bruteForce(int A[], int n, int k)
 	{
-		 int n = A.length;
-		 int remFreq[] = new int[k];
-		 int cumSum = 0;
-		 int countSub  = 0;
-		 Arrays.fill(remFreq, 0);
-		 remFreq[0] = 1;
-		 
-		 for(int i=0;i<n;i++)
-		 {
-			 cumSum += A[i];
-			 // here ((cumSum % k)+k)%k this is used to hanlde 
-			 // negative number (it is rule in modular arithmetic)
-			 remFreq[((cumSum % k)+k)%k]++; 
-		 }
-		 
-		 // once all the occurance of 
-		 // similiar remainders are counted
-		 // then for each remFreq we can choose 
-		 // any two point to get total sub aray count
-		 
-		 for(int i=0;i<k;i++)
-		 {
-			 if(remFreq[i] > 1)
-				 countSub += remFreq[i] * (remFreq[i] -1)/2; 
-		 }
-		 out.println(countSub);
-	}
-	
-	
-	// Time Complexity  : O(n^2)
-	// Space Complexity : O(1)
-	
-	static void bruteForce(int A[], int k)
-	{
-		int n = A.length;
 		int count = 0;
 		int sum = 0;
 		
 		for(int i=0; i<n; i++)
 		{
-			sum = A[i];
-			for(int j=i+1; j<n; j++)
+			for(int j=i; j<n; j++)
 			{
-				if(sum % k == 0)
-					count++;
 				sum += A[j];
+				if(((sum%k)+k)%k == 0)
+					count++;
 			}
 			
 			if(sum %k == 0)
@@ -74,5 +31,50 @@ public class CountSubarraysWithSumDivisibleByK
 		}
 		
 		out.println("\n"+count);
+	}
+	
+	
+	static void countSubArrays(int A[], int n, int k)
+	{
+		/*
+		* The map stored remainder and its frequency
+ 		*/
+		
+		Map<Integer, Integer> map =  new HashMap<>();
+		
+		/*
+		* initialy sum  = 0 and remainder is 0
+		* store 0 and frequency as 1 in the map
+		*/
+		int sum = 0;
+		map.put(0, 1);
+		// subarray count
+		int count = 0;
+		
+		for(int i=0; i<n; i++)
+		{
+			sum += A[i];
+			int rem = ((sum%k)+k)%k;
+			
+			/*
+			* Check if this remainder 
+			* already present update count
+			*/
+			if(map.containsKey(rem))
+				count += map.get(rem);
+			/*
+			* Update frequerncy of remainder in the map
+			*/
+			map.put(rem, map.getOrDefault(rem, 0)+1);
+		}
+		
+		out.println(count);
+	}
+	
+	public static void main(String [] args)
+	{   int A[] = {4, 5, 0, -12, -23, 1};
+		int k = 5;
+	    //countSubArrays(A, A.length, k);
+	    bruteForce(A, A.length, k);
 	}
 }
