028696076bb3daf110183cf6339384f18c42afae
shubhammaurya <pnssoftwares7@gmail.com>
2021-09-19 05:54:59 +0530

    Updated

---
+++ Heaps & PriorityQueue/SumOfElementsBetweenK1thSmallestAndK2thSmallest.java
@@ -0,0 +1,59 @@
+/*
+* T(n) : O(n) 
+* S(n) : O(n)
+*************
+* finding sum of elemnts between 
+* k1-th smallest and k2-th smallest 
+* k1th and k2th elemnts are exclusive
+*/
+
+import static java.lang.System.*;
+import java.util.*;
+
+public class SumOfElementsBetweenK1thSmallestAndK2thSmallest 
+{
+	 static int getSum(int[] A, int n, int k1, int k2)
+	 {
+		  int x = Math.max(k1, k2);
+		  int y = Math.min(k1, k2);
+		  
+		  // Max Heap
+		  Queue<Integer> q = new PriorityQueue<>((a,b)->{
+			  return b-a;
+		  });
+		  
+		  int i = 0, j = 0;
+		  
+		  while(j<n)
+		  {
+			 q.add(A[j]);
+			 
+			 if(j-i == x)
+			 {
+				  q.remove();
+				  i++;
+			 }
+			 
+			 j++;
+		  }
+		  
+		  int sum = 0;
+		  
+		  // remove xth smallest
+		  q.remove();
+		  
+		  // find sum till yth smallest encountered
+		  while(q.size() != y)
+			  sum += q.remove();
+		  return sum;
+	 }
+	
+	public static void main(String [] args)
+	{
+		int A[] = {10, 2, 50, 12, 48, 13};
+		int n = A.length;
+		int k1 = 2;
+		int k2 = 6;
+		out.println(getSum(A, n, k1, k2));
+	}
+}
