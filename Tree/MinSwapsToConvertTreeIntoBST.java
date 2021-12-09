03544a8a13b513e22f48280eb08b4cd3027bc1a2
mauryask <mauryask1732@gmail.com>
2021-12-10 04:22:45 +0530

    Updated

--- Tree/MinSwapsToConvertTreeIntoBST.java
+++ Tree/MinSwapsToConvertTreeIntoBST.java
@@ -82,17 +82,14 @@ public class MinSwapsToConvertTreeIntoBST
 	
 	public static void main(String[] args)
 	{
-		  Node root = new Node(3);
-		  Node r2 = new Node(12);
-		  Node r3 = new Node(2);
-		  Node r4 = new Node(4);
-		  Node r5 = new Node(12);
-		  Node r6 = new Node(25);
-		  Node r7 = new Node(10);
-		  Node r8 = new Node(14);
-		  Node r9 = new Node(23);
-		  Node r10 = new Node(52);
-		  Node r11 = new Node(7);
+		  Node root = new Node(5);
+		  Node r2 = new Node(6);
+		  Node r3 = new Node(7);
+		  Node r4 = new Node(8);
+		  Node r5 = new Node(9);
+		  Node r6 = new Node(10);
+		  Node r7 = new Node(11);
+		
 
 		  root.left = r2;
 		  root.right = r3;
@@ -100,11 +97,7 @@ public class MinSwapsToConvertTreeIntoBST
 		  r2.right = r5;
 		  r3.left = r6;
 		  r3.right = r7;
-		  r4.right = r8;
-		  r8.right = r10;
-		  r7.left = r9;
-		  r9.left = r11;
-		  
+		 		  
 		  out.println(minSwaps(root));
 	}
 }
