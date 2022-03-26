530f599b311e69ceacd38d95b56db4a67526bf40
Shubham <pnssoftwares7@gmail.com>
2022-03-26 23:31:22 +0530

    Updated

---
+++ LinkedList/AddOneToALinkedList.java
@@ -0,0 +1,86 @@
+/*
+* T(n) : O(n)
+* S(n) : O(1)
+** Add one to a number 
+** given as a linkelist
+** here i used recursion to solve this
+*/
+import static java.lang.System.*;
+import java.util.*;
+
+class Node 
+{
+    int data;
+    Node next;
+
+	Node(int data)
+	{
+		this.data = data;
+	}  
+}
+
+
+public class AddOneToALinkedList
+{
+	// this is the value to be added
+	static int adder = 1;
+	
+	static void addOne(Node head)
+	{
+	   	if(head ==  null)
+			return;
+		
+		Node nextHead =  head.next;
+		
+		addOne(nextHead);
+		
+		int sum = head.data + adder;
+		
+		head.data = sum % 10;
+		adder = sum / 10;
+	}
+	
+	
+	static void printList(Node head)
+	{
+		while(head != null)
+		{
+			out.print(head.data+" ");
+			head = head.next;
+		}
+	 
+	    out.println();
+	}
+	
+	public static void main(String [] args)
+	{
+		Node head =  new Node(9);
+		Node a =  new Node(9);
+		Node b =  new Node(9);
+		Node c =  new Node(9);
+		Node d =  new Node(9);
+		Node e =  new Node(9);
+		
+		head.next = a;
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+		
+		printList(head);
+		
+		addOne(head);
+		
+		// at the end check if the 
+		// the adder is one (the carry from 
+		// first element)
+		if(adder == 1)
+		{
+			Node temp  = new Node(1);
+			temp.next = head;
+			head = temp;
+		}
+		
+		printList(head);
+	}
+}
