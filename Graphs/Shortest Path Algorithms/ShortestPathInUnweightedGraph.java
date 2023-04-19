60991bf87b9c44b5ac1ebae5040daf6aab3aa99f
Shubham Maurya <mauryask1732@gmail.com>
2023-04-19 10:25:18 +0530

    Updated

--- Graphs/Shortest Path Algorithms/ShortestPathInUnweightedGraph.java
+++ Graphs/Shortest Path Algorithms/ShortestPathInUnweightedGraph.java
@@ -11,7 +11,8 @@ import java.util.*;
 public class ShortestPathInUnweightedGraph
 {
     List<Integer> adj[];
-	public Graph(int n)
+	
+	public ShortestPathInUnweightedGraph(int n)
 	{
 		adj = new LinkedList[n];
 		for(int i=0; i<n; i++)
@@ -34,46 +35,18 @@ public class ShortestPathInUnweightedGraph
 	}
 	
 	
-	void shortestPath(int n, int src, int dest)
+    void shortestPath(int n, int src, int dest)
 	{
-	  int parent[] = new int[n];
-	  int distance[] = new int[n];
-	  
-	  for(int i=0; i<n; i++)
-	  {
-		  parent[i] = -1;
-		  distance[i] = Integer.MAX_VALUE;
-	  }
-	  
-	   shortestPathUtil(n, src, dest, distance, parent);
-	  
-       Stack<Integer> stack = new Stack<>();
-       stack.push(dest);
-       int temp = parent[dest];
-	   
-       while(temp != -1)
-	   {
-		   stack.push(temp);
-		   temp = parent[temp];
-	   }
-      	
-       for(;!stack.isEmpty();)
-		out.print(stack.pop()+" ");		   
-	}
-	
-	
-	void shortestPathUtil(int n, int src, int dest, int distance[], int parent[])
-	{
-		List<Integer> q = new LinkedList<>();
-		boolean visited[] = new boolean[n];
-		
-		q.add(src);
+		boolean[] visited = new boolean[n];
+		Queue<Integer> q = new ArrayDeque<>();
+		int parent[] = new int[n];
+		Arrays.fill(parent, -1);
 		visited[src] = true;
-		distance[src] = 0;
+		q.add(src);
 		
 		while(!q.isEmpty())
 		{
-			int u = q.remove(0);
+			int u = q.poll();
 			
 			for(int v : adj[u])
 			{
@@ -81,20 +54,26 @@ public class ShortestPathInUnweightedGraph
 				{
 					visited[v] = true;
 					q.add(v);
-					distance[v] = distance[u] + 1;
 					parent[v] = u;
-					
-					if(v == dest)
-						return;
 				}
 			}
 		}
+		
+		int temp = dest;
+		StringBuilder sb = new StringBuilder();
+		while(temp != -1)
+		{
+			sb.insert(0, temp+" ");
+			temp = parent[temp];
+		}
+		
+		out.println(sb);
 	}
 	
 	public static void main(String [] args)
 	{
 		int n = 7;
-		Graph g = new Graph(n);
+		ShortestPathInUnweightedGraph g = new ShortestPathInUnweightedGraph(n);
 		g.addEdge(0,1);
 		g.addEdge(0,4);
 		g.addEdge(4,5);
@@ -102,7 +81,6 @@ public class ShortestPathInUnweightedGraph
 		g.addEdge(1,2);
 		g.addEdge(2,3);
 		g.addEdge(5,6);
-		out.println(g);
 		
 		g.shortestPath(n, 0, 2);		
 	}
