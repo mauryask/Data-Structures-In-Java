3477ac067250a337e788a5e8f284b89bf0f4ab58
mauryask <mauryask1732@gmail.com>
2023-06-07 21:23:40 +0530

    Updated

--- Graphs/Graph Reprsentation/GraphUsingAdjacencyMatrix.java
+++ Graphs/Graph Reprsentation/GraphUsingAdjacencyMatrix.java
@@ -5,6 +5,7 @@
 **/
 
 import java.util.*;
+
 public class GraphUsingAdjacencyMatrix 
 {
 	private static int v;
--- Searching/ExponentialSearch.java
+++ Searching/ExponentialSearch.java
@@ -1,37 +1,36 @@
-// Time complexity : O(log i) > here i is index of element to be searched
+// T(n) : O(log i) : i is index of the last pointed element
+//S(n) : O(1)
 // This is useful in case when array is unbounded (tends to infinite)
 
 import java.util.Arrays;
+import static java.lang.System.*;
+
 public class ExponentialSearch 
 {
     static int exponentialSearch(int A[], int item)
 	{
 		int i = 1;
+		
 		if(A[0] == item)
 		  return 0;
 	  
-		while(i < A.length && A[i] <= item) //O(log i) > "i" is index of searched element
+	    //Finding the range where the element might be found
+		while(i < A.length && A[i] <= item)  // O(log i)
 		{
 		     if(A[i] == item)
 				 return i;
+			 
 			 i = i * 2;
 		}
 		
-		// it returns a negative nuber if element is not found in the array
-		/** 
-		* use 'A.length' and 'i+1' instead of 
-		* 'A.length-1' and 'i' beacause it is exclusive
-		*/
-	    return Arrays.binarySearch(A,i/2,Math.min(i+1,A.length),item); //O(log i)
+		//O(log i)
+	    return Arrays.binarySearch(A,i/2,Math.min(i,A.length-1),item);
 	}
 		
 	public static void main(String [] args)
 	{
-		int A[] = {5,6,7,10,23,54,45,78,4,0,23,645,8,52,156,78};
-		Arrays.sort(A);
-		for(int x : A)
-		System.out.print(x+" ");
-		int item = 45;
-		System.out.print("\n"+exponentialSearch(A,item));		
+		int A[] = {0,4,5,6,7,8,10,23,23,45,52,54,78,78,156,645};
+		int target = 45;
+		out.print(exponentialSearch(A,target));		
 	}
 }
