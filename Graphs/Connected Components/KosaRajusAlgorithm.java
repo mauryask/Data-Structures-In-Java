2ee1cf0fd5d569e05de0f65039cf37a36fcca01d
Shubham <mauryask1732@gmail.com>
2024-06-17 10:45:00 +0530

    Update KosaRajusAlgorithm.java

--- Graphs/Connected Components/KosaRajusAlgorithm.java
+++ Graphs/Connected Components/KosaRajusAlgorithm.java
@@ -10,104 +10,99 @@ import java.util.*;
 
 public class KosaRajusAlgorithm
 {
-    static void dfs1(int u, ArrayList<ArrayList<Integer>> adj,
-	Stack<Integer> stack, boolean visited[]){
-        visited[u] = true;
-        
-        for(int v : adj.get(u))
-        {
-            if(!visited[v])
-            {
-                dfs1(v, adj, stack, visited);
-            }
-        }
-        
-        stack.push(u);
-    }
-    
-    
-    static void dfs2(int u, ArrayList<ArrayList<Integer>> adj,
-	boolean[] visited)
-    {
-        visited[u] = true;
-        
-        for(int v : adj.get(u))
-        {
-            if(!visited[v])
-            {
-                dfs2(v, adj, visited);
-            }
-        }
-    }
-    
-    static ArrayList<ArrayList<Integer>> reverse(ArrayList<ArrayList<Integer>> adj, int V)
-    {
-        ArrayList<ArrayList<Integer>> revAdj = new ArrayList<>();
-        
-        for(int i=0; i<V; i++)
-           revAdj.add(new ArrayList<>());
-          
-        for(int i=0; i<V; i++)
-        {
-            for(int u : adj.get(i))
-                revAdj.get(u).add(i);
-        }
-        
-        return revAdj;
-    }
-    
-    static int kosaraju(int V, ArrayList<ArrayList<Integer>> adj)
-    {
-        boolean visited[] =  new boolean[V];
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0; i<V; i++)
-        {
-            if(!visited[i])
-              dfs1(i, adj, stack, visited);
-        }
-        
-        adj = reverse(adj, V);
-        
-        Arrays.fill(visited, false);
-        
-        int count = 0;
-        
-        while(!stack.isEmpty())
-        {
-             int v = stack.pop();
-             
-             if(!visited[v])
-             {
-                count++;
-                dfs2(v, adj, visited);
-             }
-        }
-        
-        return count;
-    }
-	
-	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v)
+   	static List<List<Integer>> getScc(List<ArrayList<Integer>> graph, int n)
 	{
-		adj.get(u).add(v);
+		  Stack<Integer> stack = new Stack<>();
+		  boolean visited[] = new boolean[n];
+		  
+		  for(int i=0; i<n; i++)
+		  {
+			  if(!visited[i])
+				  fillOrder(i, graph, stack, visited);
+		  }
+		  
+		  graph = getTranspose(graph, n);
+		  
+		  Arrays.fill(visited, false);
+		  int count = 0;
+		  
+		  List<List<Integer>> ans = new ArrayList<>();
+		  
+		  while(!stack.isEmpty())
+		  {
+			  int u  = stack.pop();
+			  if(!visited[u])
+			  {
+				  count++;
+				  List<Integer> scc = new ArrayList<>();
+				  findScc(u, graph, visited, scc);
+				  ans.add(scc);
+			  }
+		  }
+		  
+		  return ans;
+	}
+		
+	// O(V+E)
+	static void fillOrder(int u, List<ArrayList<Integer>> graph, Stack<Integer> stack, boolean visited[])
+	{
+	   visited[u] = true;
+
+       for(int v : graph.get(u))
+		   if(!visited[v])
+			   fillOrder(v, graph, stack, visited);
+       stack.push(u);		   
+	}
+	
+	// O(V+E)
+	static List<ArrayList<Integer>> getTranspose(List<ArrayList<Integer>> graph, int n)
+	{
+		List<ArrayList<Integer>> g = 
+		new ArrayList<ArrayList<Integer>>();
+		
+		for(int i=0; i<n; i++)
+			g.add(new ArrayList<Integer>());
+		
+		for(int i=0; i<n; i++)
+		{
+		    for(int u : graph.get(i))
+				g.get(u).add(i);
+		}
+		
+		return g;
+	}
+	
+	// O(V+E)
+	static void findScc(int u, List<ArrayList<Integer>> graph, boolean visited[], List<Integer> scc)
+	{
+		visited[u] = true;
+		scc.add(u);
+		for(int v : graph.get(u))
+			if(!visited[v])
+				findScc(v, graph, visited, scc);
 	}
 	
 	public static void main(String [] args)
 	{
-		int n = 6;		
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+		Scanner sc =  new Scanner(in);
+		out.print("Enter number of vertices: ");
+		int n = sc.nextInt();
+		out.print("Enter number of edges: ");
+		int e = sc.nextInt();
+		
+        List<ArrayList<Integer>> graph =  
+		new ArrayList<ArrayList<Integer>>();
 		
 		for(int i=0; i<n; i++)
-			adj.add(new ArrayList<>());
-						
-        addEdge(adj, 0, 1);			
-        addEdge(adj, 1, 2);			
-        addEdge(adj, 2, 0);			
-        addEdge(adj, 2, 3);			
-        addEdge(adj, 3, 4);			
-        addEdge(adj, 4, 5);			
-        addEdge(adj, 5, 3);			
+			graph.add(new ArrayList<Integer>());
+		out.println("Enter edges u-->v: ");
+        while(e-->0)
+		{			
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			graph.get(u).add(v);
+		}			
 		
-		out.println(kosaraju(n, adj));
+		out.println(getScc(graph, n));
 	}
 }
