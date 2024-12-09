bdabc5eb4594d7768e1e62070e68633b05bf3540
mauryask <mauryask1732@gmail.com>
2024-12-10 00:44:15 +0530

    Floyd warshalls algorithm improved

--- Graphs/Shortest Path Algorithms/FloydWarShallsAlgorithm.java
+++ Graphs/Shortest Path Algorithms/FloydWarShallsAlgorithm.java
@@ -10,58 +10,47 @@
 ** Note: Floyd Warshall's Algorithm does not work for graphs with -ve edge weight cycle
 */
 
-class AllPairShortestPath
-{
-	static final int inf = Integer.MAX_VALUE;
-	
-	void floydWarshall(int G[][], int v_num)
-	{		
-			for(int k=0; k<v_num; k++)
-			{
-				for(int i=0; i<v_num; i++)
-				{
-					for(int j=0; j<v_num; j++)
-					{
-						if(G[i][k] != inf && G[k][j] != inf)
-							G[i][j] = Math.min(G[i][j], G[i][k] 
-						+ G[k][j]);
-					}
+class AllPairShortestPath {
+	private static final int inf = Integer.MAX_VALUE;
+
+	void floydWarshall(int G[][], int v_num) {
+		for (int k = 0; k < v_num; k++) {
+			for (int i = 0; i < v_num; i++) {
+				for (int j = 0; j < v_num; j++) {
+					if (G[i][k] != inf && G[k][j] != inf && G[i][k] + G[k][j] < G[i][j])
+						G[i][j] = G[i][k] + G[k][j];
 				}
 			}
+		}
 	}
-	
-	void printDist(int dist[][], int v_num)
-	{
-		for(int i=0; i<v_num; i++)
-		{
-			for(int j=0; j<v_num; j++)
-			{
-				System.out.println(i+" ==> "+j+" = "+dist[i][j]);
+
+	void printDist(int dist[][], int v_num) {
+		for (int i = 0; i < v_num; i++) {
+			for (int j = 0; j < v_num; j++) {
+				System.out.println(i + " ==> " + j + " = " + dist[i][j]);
 			}
-			
+
 			System.out.println("\n===============\n");
 		}
 	}
 }
 
-public class FloydWarShallsAlgorithm
-{
+public class FloydWarShallsAlgorithm {
 	static final int inf = Integer.MAX_VALUE;
-	
-	public static void main(String [] args)
-	{
+
+	public static void main(String[] args) {
 		AllPairShortestPath a = new AllPairShortestPath();
 		int v_num = 4;
-		//create a n*n matrix : n is number of vertices
+		// create a n*n matrix : n is number of vertices
 		// It is a directed graph
 		int graph[][] = {
-							{0, 5, inf, 10},
-							{inf,0 , 3, inf},
-							{inf, inf, 0, 1},
-							{inf, inf, inf, 0}
-		                };
-		
-		a.floydWarshall(graph,v_num);
+				{ 0, 5, inf, 10 },
+				{ inf, 0, 3, inf },
+				{ inf, inf, 0, 1 },
+				{ inf, inf, inf, 0 }
+		};
+
+		a.floydWarshall(graph, v_num);
 		a.printDist(graph, v_num);
 	}
 }
