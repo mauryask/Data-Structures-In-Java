9a67a65e9566c1e174228ce6df13d8acf573322b
mauryask <mauryask1732@gmail.com>
2025-01-27 23:22:22 +0530

    Merge k sorted linked lists optimized approach added

--- LinkedList/MergekSortedLinkedLists.java
+++ LinkedList/MergekSortedLinkedLists.java
@@ -1,71 +1,92 @@
+
+import java.util.*;
+
 //T(n) : O(nk)
 //S(n) : O(1)
+public class MergekSortedLinkedLists {
 
-public class MergekSortedLinkedLists 
-{
-	Node mergeSortedList(Node head1, Node head2)
-    {
-        if(head1 == null)
-           return head2;
-        if(head2 == null)
-           return head1;
-           
-        Node p1 = null;
-        Node p2= null;   
-           
-        if(head1.data < head2.data)
-        {
-           p1 = head1;
-           p2 = head2;
+    Node mergeSortedList(Node head1, Node head2) {
+        // Hnadle edge cases
+        if (head1 == null) {
+            return head2;
         }
-        else
-        {
-            p1 = head2;
-            p2 = head1;
+        if (head2 == null) {
+            return head1;
         }
-           
-        Node ptr = p1;
-        Node head = p1;
-        p1 = p1.next;
-        
-        while(p1 != null && p2 != null)
-        {
-            if(p1.data < p2.data)
-            {
-                ptr.next = p1;
-                ptr = p1;
-                p1 = p1.next;
-            }
-            else
-            {
-                ptr.next = p2;
-                ptr = p2;
-                p2 = p2.next;
+
+        Node prev = null;
+        Node head = null;
+
+        if (head1.data < head2.data) {
+            prev = head1;
+            head1 = head1.next;
+        } else {
+            prev = head2;
+            head2 = head2.next;
+        }
+
+        head = prev;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                prev.next = head1;
+                prev = head1;
+                head1 = head1.next;
+            } else {
+                prev.next = head2;
+                prev = head2;
+                head2 = head2.next;
             }
         }
-        
-        if(p1 == null)
-           ptr.next = p2;
-        else 
-          ptr.next = p1;
-          
-          return head;
+
+        prev.next = head1 == null ? head2 : head1;
+
+        return head;
     }
-    
-    Node mergeKList(Node[]arr,int k)
-    {
-       if(k == 1)
-          return arr[0];
-          
-       Node head1 = arr[0];
-       Node head2 = null;
-       
-       for(int i=1; i<arr.length; i++)
-       {
-           head2 = arr[i];
-           head1 = mergeSortedList(head1, head2);
-       }
-       
-       return head1;
+
+    Node mergeKList(Node[] arr, int k) {
+        if (k == 1) {
+            return arr[0];
+        }
+
+        Node head1 = arr[0];
+        Node head2 = null;
+
+        for (int i = 1; i < arr.length; i++) {
+            head2 = arr[i];
+            //Very important consideration 
+            head1 = mergeSortedList(head1, head2);
+        }
+
+        return head1;
+    }
+
+// Optimized solution using Priority Queue 
+// T(n) : O(n * log k) : As we are adding n elements to the priority queue and each insertion will take log k time
+// S(n) : O(k) : As at any time there will be at most k elements in the priority queue
+    Node mergerKSortedLinkedLists(List<Node> arr) {
+        Queue<Node> q = new PriorityQueue<>((a, b) -> a.data - b.data);
+        Node dummy = new Node(0);
+        Node head = dummy;
+
+        for (Node node : arr) {
+            if (node != null) //Some lists can have null as head
+            {
+                q.add(node);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            dummy.next = node;
+            dummy = node; // This is kind of a pointer to the last node of the merged list
+
+            if (node.next != null) {
+                q.add(node.next);
+            }
+        }
+
+        return head.next;
     }
 }
--- Tree/fillNextSibling.java
+++ Tree/fillNextSibling.java
@@ -1,100 +1,103 @@
+
 /**
-T(n) : O(n)
-S(n) : O(n) (In case of iterative solution) and O(1) (In case of recursive solution) 
-*/
+ * T(n) : O(n)
+ * S(n) : O(n) (In case of iterative solution) and O(1) (In case of recursive solution)
+ */
 import java.util.*;
 
-class Node 
-{
-	int data;
-	Node left;
-	Node right;
-	Node next;
+class Node {
 
-	Node(int data)
-	{
-		this.data = data;
-	}
+    int data;
+    Node left;
+    Node right;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
 }
 
-public class fillNextSibling
-{
-  //Note: The iterative solution is more generic and works for all types of binary trees
-    static  Node connectNdoes(Node root) {
-        if(root == null)
-           return root;
+public class fillNextSibling {
+    //Note: The iterative solution is more generic and works for all types of binary trees
+
+    static Node connectNdoes(Node root) {
+        if (root == null) {
+            return root;
+        }
 
         Node rootNode = root;
-        
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(root);        
 
-        while(!q.isEmpty()){
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
             int count = q.size();
             Node prev = null;
 
-            while(count-->0){
+            while (count-- > 0) {
                 root = q.poll();
 
-                if(root.left != null)
-                  q.add(root.left);
+                if (root.left != null) {
+                    q.add(root.left);
+                }
 
-                if(root.right != null)
-                q.add(root.right);
+                if (root.right != null) {
+                    q.add(root.right);
+                }
 
-                if(prev != null)
-                  prev.next = root;
-                
+                if (prev != null) {
+                    prev.next = root;
+                }
+
                 prev = root;
             }
         }
 
         return rootNode;
 
-      //Comment the above code and uncomment the below code for recursive solution
-      
-       /* if(root != null) recursiveSolution(root);
+        //Comment the above code and uncomment the below code for recursive solution
+        /* if(root != null) recursiveSolution(root);
           return root;*/
     }
 
-  //Note: The recursive solution is more specific and works only for perfect binary trees
-    static void recursiveSolution(Node root){
+    //Note: The recursive solution is more specific and works only for perfect binary trees
+    static void recursiveSolution(Node root) {
         // If it is leaf node : do not make a function call
-        if(root.left != null){
+        if (root.left != null) {
             root.left.next = root.right;
-            if(root.next != null)
-               root.right.next = root.next.left;   
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
 
-            recursiveSolution(root.left);   
+            recursiveSolution(root.left);
             recursiveSolution(root.right);
         }
     }
-	
-	public static void main(String [] args)
-	{
-		  Node root = new Node(1);
-		  Node r2 = new Node(2);
-		  Node r3 = new Node(3);
-		  Node r4 = new Node(4);
-		  Node r5 = new Node(5);
-		  Node r6 = new Node(6);
-		  Node r7 = new Node(7);
-		  Node r8 = new Node(8);
-		  Node r9 = new Node(9);
-		  Node r10 = new Node(10);
-		  Node r11 = new Node(11);
-		  Node r12 = new Node(12);
-		  Node r13 = new Node(13);
-		  Node r14 = new Node(14);
-		  Node r15 = new Node(15);
-		  
-		  root.left = r2;
-		  root.right = r3;
-		  r2.left = r4;
-		  r2.right = r5;
-		  r3.left = r6;
-		  r3.right = r7;
-		 /* r4.left = r8;
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node r2 = new Node(2);
+        Node r3 = new Node(3);
+        Node r4 = new Node(4);
+        Node r5 = new Node(5);
+        Node r6 = new Node(6);
+        Node r7 = new Node(7);
+        Node r8 = new Node(8);
+        Node r9 = new Node(9);
+        Node r10 = new Node(10);
+        Node r11 = new Node(11);
+        Node r12 = new Node(12);
+        Node r13 = new Node(13);
+        Node r14 = new Node(14);
+        Node r15 = new Node(15);
+
+        root.left = r2;
+        root.right = r3;
+        r2.left = r4;
+        r2.right = r5;
+        r3.left = r6;
+        r3.right = r7;
+        /* r4.left = r8;
 		  r4.right = r9;
 		  r5.left = r10;
 		  r5.right = r11;
@@ -103,6 +106,6 @@ public class fillNextSibling
 		  r7.left = r14;
 		  r7.right = r15;*/
 
-		  connectNdoes(root);
-	}
+        connectNdoes(root);
+    }
 }
