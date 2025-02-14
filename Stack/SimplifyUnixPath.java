5cfe3eb71cbcda64cc0c8622d7d8747e17f46cf5
mauryask <mauryask1732@gmail.com>
2025-02-14 17:57:40 +0530

    Updated

--- Stack/SimplifyUnixPath.java
+++ Stack/SimplifyUnixPath.java
@@ -2,47 +2,50 @@
 * T(n) : O(n) : n is length of the string
 * S(n) : O(n)
 * all the cases are passed at leet code
-*/
+ */
 import static java.lang.System.*;
 import java.util.*;
 
-public class SimplifyUnixPath 
-{
-	static String simplePath(String path)
-	{
-		String[] tokens = path.split("/+"); 
-		Stack<String> stack = new Stack<>();
-	    						
-		for(String token : tokens)
-		{
-			if(token.equals("..") && !stack.isEmpty())
-				stack.pop();
-			else if(!token.equals(".") && !token.equals(""))
-				stack.push(token);
-		}			
-		
-		StringBuilder sb = new StringBuilder();
-		for(String str : stack)
-		  sb.append("/"+str);
-		
-		/*
+public class SimplifyUnixPath {
+
+    static String simplePath(String path) {
+        String[] tokens = path.split("/+");
+        Stack<String> stack = new Stack<>();
+
+        //Start fron index 1 as first index is always gouing to be empty								
+        for (int i = 1; i < tokens.length; i++) {
+            String token = tokens[i];
+            if (token.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!token.equals(".")) {
+                stack.push(token);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : stack) {
+            sb.append("/").append(str);
+        }
+
+        /*
 		* check if stack is empty for the case: /////
-		*/
-		return stack.isEmpty() ? "/" : sb.toString();
-	}
-	
-	public static void main(String [] args)
-	{
-		 String path = /*"/../";
-		 */
-		  "/a/./b/../c/./d/";/*
+         */
+        return stack.isEmpty() ? "/" : sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String path
+                = /*"/../";
+                 */ "/a/./b/../c/./d/";/*
 		 * "/../../../../../a";
 		 * "////"; 
-		 */
-		 
-		 /*String str[]  = path.split("/+");
+         */
+
+ /*String str[]  = path.split("/+");
 		 for(String s : str)
 			 out.println(s);*/
-	     out.println(simplePath(path));
-	}
+        out.println(simplePath(path));
+    }
 }
