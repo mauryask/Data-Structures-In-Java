941e13ee4760f5eb5ed0428960ee54556fe63b8f
mauryask <mauryask1732@gmail.com>
2025-02-15 23:40:01 +0530

    Max Area rectangle under histogram algorithm improved

--- Stack/TrappingRainWaterBestWay.java
+++ Stack/TrappingRainWaterBestWay.java
@@ -1,12 +1,22 @@
 /*
 * T(n) : O(n)
 * S(n) : O(1)
+** the idea is if leftHeight > rightHeight >> We know edge buildings between which water 
+** wilol be trapped for example left is 4 and right is 3
+** We know that water can be trapped on current building
+** rightMax - current building height 
 */
 public class TrappingRainWaterBestWay 
 {
     public static int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxLeft = 0, maxRight = 0;
+        int n = height.length;
+        if(n< 3)
+          return 0;
+
+        //Initilize from 1 and n-1 cause water can not be stored on edge buildings  
+        int left = 1, right = height.length - 2;
+        //Assume edge elements as largest for now
+        int maxLeft = height[0], maxRight = height[n-1];
         int trappedWater = 0;
 
         while (left <= right) {
@@ -15,7 +25,7 @@ public class TrappingRainWaterBestWay
                 maxLeft = Math.max(maxLeft, height[left]);
                 trappedWater += maxLeft - height[left];
                 left++;
-            } else {
+            } else { 
                 // Update max height seen from right
                 maxRight = Math.max(maxRight, height[right]);
                 trappedWater += maxRight - height[right];
