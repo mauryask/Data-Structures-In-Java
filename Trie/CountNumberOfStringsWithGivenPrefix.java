7ba7d4eeb8b3c940875c9b1fbd3462f43d8c02a2
mauryask <mauryask1732@gmail.com>
2025-02-16 23:55:47 +0530

    Prfix word suggestion logic fixed

--- Trie/CountNumberOfStringsWithGivenPrefix.java
+++ Trie/CountNumberOfStringsWithGivenPrefix.java
@@ -1,62 +1,58 @@
+
 import static java.lang.System.*;
 import java.util.*;
 
-class TrieNode 
-{
-	// prefix-count 
-	int pc = 0; 
-	TrieNode chArr[]  = new TrieNode[26];
+class TrieNode {
+    // prefix-count 
+
+    int pc = 0;
+    TrieNode chArr[] = new TrieNode[26];
 }
 
-public class CountNumberOfStringsWithGivenPrefix
-{	
-   // T(n) : O(word-length)
-	static void  insertWord(Node root, String word)
-	{
-		char [] arrChar = word.toCharArray();
-		
-		for(char ch : arrChar)
-		{			
-			if(root.chArr[ch-'a'] == null)
-			{
-				Node temp = new Node();
-				root.chArr[ch-'a'] = temp;
-				root = temp; 
-			}
-		    else
-              root = root.chArr[ch-'a'];
-		  
-           root.pc++;		  
-		}	
-	}	
-	
-	// T(n) : O(prefix-length)
-	static int countString(Node root, String prefix)
-	{
-		char arrChar[] = prefix.toCharArray();
-        
-		for(char ch : arrChar)
-		{
-		   if(root.chArr[ch-'a'] != null)
-			   root = root.chArr[ch-'a'];
-		   else
-			  return 0;
-		}
-				
-		return root.pc;
-	}
-		
-	public static void main(String [] args)
-	{
-		Node root = new Node();
-		
-		insertWord(root, "abc");
-		insertWord(root, "bca");
-		insertWord(root, "ab");
-		insertWord(root, "fcda");
-		insertWord(root, "pfet");
-		insertWord(root, "flex");
+public class CountNumberOfStringsWithGivenPrefix {
+    // T(n) : O(word-length)
 
-		out.println(countString(root, "a"));
-	}
+    static void word(TrieNode root, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (root.chArr[ch - 'a'] == null) {
+                TrieNode temp = new TrieNode();
+                root.chArr[ch - 'a'] = temp;
+                root = temp;
+            } else {
+                root = root.chArr[ch - 'a'];
+            }
+            // for each characer increse this value
+            root.pc++;
+        }
+    }
+
+    // T(n) : O(prefix-length)
+    static int countString(TrieNode root, String prefix) {
+        char arrChar[] = prefix.toCharArray();
+
+        for (char ch : arrChar) {
+            if (root.chArr[ch - 'a'] != null) {
+                root = root.chArr[ch - 'a'];
+            } else {
+                return 0;
+            }
+        }
+
+        return root.pc;
+    }
+
+    public static void main(String[] args) {
+        TrieNode root = new TrieNode();
+
+        word(root, "abc");
+        word(root, "bca");
+        word(root, "ab");
+        word(root, "fcda");
+        word(root, "pfet");
+        word(root, "flex");
+
+        out.println(countString(root, "fcd"));
+    }
 }
