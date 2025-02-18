bf6646ebb482d288457f447c8ef210402c6c017e
mauryask <mauryask1732@gmail.com>
2025-02-19 00:03:38 +0530

    Updated

--- LinkedList/AddTwoNumbersInALinkedListsRecursive.java
+++ LinkedList/AddTwoNumbersInALinkedListsRecursive.java
@@ -100,3 +100,91 @@ public class AddTwoNumbersInALinkedListsRecursive {
         printList(head);
     }
 }
+
+
+
+/*
+
+// Another good approch without reversing the lists
+
+class Solution {
+    static int carry = 0;
+    
+    static Node sum(Node head1, Node head2){
+        if(head1 == null && head2 == null)
+          return null;
+          
+        Node nextNode1 = head1 == null ? null : head1.next;
+        Node nextNode2 = head2 == null ? null : head2.next;
+        
+        Node prev = sum(nextNode1, nextNode2);
+        
+        int val1 = head1 == null ? 0 : head1.data;
+        int val2 = head2 == null ? 0 : head2.data;
+        
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        
+        Node head = new Node(sum % 10);
+        head.next = prev;
+        
+        return head;
+    }
+    
+    static int getLength(Node head){
+        int count = 0;
+        
+        while(head != null){
+            count++;
+            head = head.next;
+        }
+        
+        return count;
+    }
+    
+    static Node removeLeadingZeros(Node head){
+        while(head != null && head.data == 0){
+            head = head.next;
+        }
+        
+        return head;
+    }
+    
+    static Node addTwoLists(Node head1, Node head2) {
+           if(head1 == null) return head2;
+           if(head2 == null) return head1;
+           
+           head1 = removeLeadingZeros(head1);
+           head2 = removeLeadingZeros(head2);
+           
+           int l1 = getLength(head1);
+           int l2 = getLength(head2);
+           int steps = Math.abs(l1 - l2); 
+          
+           if(l2 > l1){
+               Node temp = head2;
+               head2 = head1;
+               head1 = temp;
+           }
+           
+           while(steps-->0){
+               Node temp = new Node(0);
+               temp.next = head2;
+               head2 = temp;
+           }
+           
+           carry =  0;
+           
+           Node head = sum(head1, head2);
+           
+           if(carry > 0){
+               Node temp =  new Node(carry);
+               temp.next = head;
+               head = temp;
+           }
+           
+           return head;
+    }
+}
+
+**/
