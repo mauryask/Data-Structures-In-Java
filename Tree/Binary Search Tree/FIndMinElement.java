b654e39cb9a8dfddec4e24fd0280e3a4e83020af
mauryask <mauryask1732@gmail.com>
2025-02-21 23:50:49 +0530

    Updated

--- Tree/Binary Search Tree/FIndMinElement.java
+++ Tree/Binary Search Tree/FIndMinElement.java
@@ -1,102 +1,100 @@
+
 /**
-Time complexity  : O(log n)
-Space complexity : O(1)
-*/
+ * Time complexity  : O(log n)
+ * Space complexity : O(1)
+ */
 
 import static java.lang.System.*;
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int data)
-	{
-		this.data = data;
-		left = null;
-		right = null;
-	}
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
 
-public class FIndMinElement
-{
-	static Node root = null;
-	
-	static void insertNode(int data)
-	{
-		Node temp = new Node(data);
-		Node currentNode = null, parentNode = null;
-		
-		if(root == null)
-			root = temp;
-		else
-		{
-			currentNode = root;
-			while(currentNode!= null)
-			{
-				parentNode = currentNode;
-				if(currentNode.data < temp.data)
-					currentNode = currentNode.right;
-				else if(currentNode.data > temp.data)
-					currentNode = currentNode.left;
-			}
-			
-			if(parentNode.data > temp.data)
-				parentNode.left = temp;
-			else if(parentNode.data < temp.data)
-				parentNode.right = temp;
-		}
-	}
-	
-	static void inorder(Node root)
-	{
-		if(root != null)
-		{
-			inorder(root.left);
-			out.print(root.data+" ");
-			inorder(root.right);
-		}
-	}
-	
-	// S(n) = O(1) 
-	static int findMinElement(Node root)
-	{
-		if(root == null)
-			return -1;
-		
-		while(root.left != null)
-			root = root.left;
-		
-		return root.data;
-	}
-	
-	// S(n) = O(n) : recursion stack
-	static Node findMinRecursive(Node root)
-	{
-		if(root == null)
-			return null;
-		
-		if(root.left == null)
-			return root;
+public class FIndMinElement {
 
-		return findMinRecursive(root.left);
-	}
-	
-	public static void main(String [] args)
-	{
-		insertNode(89);
-		insertNode(36);
-		insertNode(78);
-		insertNode(120);
-		insertNode(100);
-		insertNode(45);
-		insertNode(145);
-		
-		inorder(root);
-		out.println();
-		out.println(findMinElement(root));
-		out.println(findMinRecursive(root).data);
-		
-		}
+    static Node root = null;
+
+    static void insertNode(int data) {
+        Node temp = new Node(data);
+        Node currentNode = null, parentNode = null;
+
+        if (root == null) {
+            root = temp; 
+        }else {
+            currentNode = root;
+            while (currentNode != null) {
+                parentNode = currentNode;
+                if (currentNode.data < temp.data) {
+                    currentNode = currentNode.right; 
+                }else if (currentNode.data > temp.data) {
+                    currentNode = currentNode.left;
+                }
+            }
+
+            if (parentNode.data > temp.data) {
+                parentNode.left = temp; 
+            }else if (parentNode.data < temp.data) {
+                parentNode.right = temp;
+            }
+        }
+    }
+
+    static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    // S(n) = O(1) 
+    static int findMinElement(Node root) {
+        if (root == null) {
+            return -1;
+        }
+
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root.data;
+    }
+
+    // S(n) = O(n) : recursion stack
+    static int findMinRecursive(Node root) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.left == null) {
+            return root.data;
+        }
+
+        return findMinRecursive(root.left);
+    }
+
+    public static void main(String[] args) {
+        insertNode(89);
+        insertNode(36);
+        insertNode(78);
+        insertNode(120);
+        insertNode(100);
+        insertNode(45);
+        insertNode(145);
+
+        inorder(root);
+        out.println();
+        // out.println(findMinElement(root));
+        out.println(findMinRecursive(root));
+
+    }
 }
