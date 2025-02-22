912dea50776d77cd8ed4496583292c0a3ec4100e
mauryask <mauryask1732@gmail.com>
2025-02-22 12:02:03 +0530

    Updated

--- Tree/BurnTheTree.java
+++ Tree/BurnTheTree.java
@@ -1,125 +1,86 @@
 /*
 * T(n) : O(n*k)
 * S(n) : O(n)
-*/
+ */
 
-import static java.lang.System.*;
 import java.util.*;
 
-class Node  
+class Node {
 
-{
-	int data;
-	Node left, right;
-	Node(int data)
-	{
-		this.data = data;
-		left = right = null;
-	}
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
 }
 
-public class BurnTheTree
-{
-   static boolean nodeToRootPath(Node root, Node target, 
-   List<Node> path)
-   {
-       if(root == null)
-           return false;
-       
-       if(root == target)
-       {
-           path.add(root);
-           return true;
-       }
-       
-       boolean left = nodeToRootPath(root.left, target, path);
-       
-       if(left)
-       {
-           path.add(root);
-           return true;
-       }
-       else
-       {
-           boolean right = nodeToRootPath(root.right, target, path);
-           if(right)
-           {
-               path.add(root);
-               return true;
-           }
-       }
-       
-       return false;
-   }
-  
-   static int maxTime =  0;
-  
-   static void storeNodeSequence(Node root, Node blocker, int time, Map<Integer, ArrayList<Integer>> map)
-   {
-       if(root == null || root == blocker)
-           return; 
-       
-	   //  recording the sequence in which the 
-       //  the nodes will be burnt 	   
-	   ArrayList<Integer> tempList = map.getOrDefault(time, new ArrayList<Integer>());
-	   tempList.add(root.data);
-	   map.put(time, tempList);
+public class BurnTheTree {
+    static boolean nodeToRootPath(Node root, int value, List<Node> list)
+    {
+        if (root == null) {
+            return false;
+        }
 
-       maxTime  = Math.max(maxTime, time);
-        
-       storeNodeSequence(root.left, blocker, time+1, map);
-       storeNodeSequence(root.right, blocker, time+1, map);
-   }
+        if (root.data == value) {
+            list.add(root);
+            return true;
+        }
 
-   static void burnTheTree(Node root, Node target)
-   {
+        boolean left = nodeToRootPath(root.left, value, list);
+        if (left) {
+            list.add(root);
+            return true;
+        }
+
+        boolean right = nodeToRootPath(root.right, value, list);
+        if (right) {
+            list.add(root);
+            return true;
+        }
+
+        return false;
+    }
+
+    static void burn(Node root, Node blocker, int time, Map<Integer, List<Node>> map) 
+    {
+        if (root == null || root == blocker) {
+            return;
+        }
+
+        List<Node> list = map.getOrDefault(time, new ArrayList<>());
+        list.add(root);
+        map.put(time, list);
+        //calculate max time here..
+        burn(root.left, blocker, time + 1, map);
+        burn(root.right, blocker, time + 1, map);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node node1 = new Node(2);
+        Node node2 = new Node(3);
+        Node node3 = new Node(4);
+        Node node4 = new Node(5);
+        Node node5 = new Node(6);
+        Node node6 = new Node(7);
+        Node node7 = new Node(8);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.right = node5;
+        node5.left = node6;
+        node6.left = node7;
+
         List<Node> path = new ArrayList<>();
-        nodeToRootPath(root,target, path); 
-		
-		Map<Integer, ArrayList<Integer>> map =  new TreeMap<>();        
-
-        for(int i=0 ; i<path.size(); i++)
-            storeNodeSequence(path.get(i), i==0? null : path.get(i-1), /*time*/i, map);
+        nodeToRootPath(root, 6, path);
         
-		for(Map.Entry<Integer, ArrayList<Integer>> m : map.entrySet())
-			System.out.println(m.getKey()+": "+m.getValue());
-		
-        out.println("totalTime: "+maxTime);
-   }
-  
-  public static void main(String [] args)
-  {
-		Node root = new Node(1);
-		Node r2 = new Node(2);
-		Node r3 = new Node(3);
-		Node r4 = new Node(4);
-		Node r5 = new Node(5);
-		Node r6 = new Node(6);
-		Node r7 = new Node(7);
-		Node r8 = new Node(8);
-		Node r9 = new Node(9);
-		Node r10 = new Node(10);
-		Node r11 = new Node(11);
-		Node r12 = new Node(12);
-		Node r13 = new Node(13);
-		Node r14 = new Node(14);
-		Node r15 = new Node(15);
-		Node r16 = new Node(16);
-		Node r17 = new Node(17);
-		
-		root.left = r2;
-		root.right = r3;
-		r2.left = r4;
-		r2.right = r5;
-		r3.left = r6;
-     	r3.right = r7;
-		r5.left = r8;
-		r5.right = r9;
-		r6.right = r10;
-		r10.left = r13;
-		r7.left = r12;
-		r7.right = r11;
-	    
-       burnTheTree(root, r13);	   
+        Map<Integer, List<Node>> map = new HashMap<>();
+
+        for (int i = 0; i < path.size(); i++) {
+            burn(path.get(i), i == 0 ? null : path.get(i - 1), i, map);
+        }
     }
 }
--- Tree/CheckIfBinaryTreeIsBalanced.java
+++ Tree/CheckIfBinaryTreeIsBalanced.java
@@ -1,68 +1,65 @@
 import static java.lang.System.*;
-import java.util.*;
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	Node(int data)
-	{
-		this.data = data;
-	   right = left = null;
-	}
+// T(n) : O(n)
+// S(n) : O(n)
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        right = left = null;
+    }
 }
 
-public class CheckIfBinaryTreeIsBalanced
-{
-	// T(n) : O(n)
-		
-	static boolean isBalanced = true;
-	
-    static int balanceBinaryTree(Node root)
-	{	
-        if(root != null && isBalanced)
-		{
-			int lHeight = balanceBinaryTree(root.left);
-			int rHeight = balanceBinaryTree(root.right);
-			int diff = Math.abs(lHeight - rHeight);
-			
-			if(diff > 1)
-				isBalanced = false;
-		    
-			return Math.max(lHeight, rHeight) + 1;
-		}			
-		
-		return 0;
-	}
-	
-	public static void main(String[] args)
-	{
-		  Node root = new Node(3);
-		  Node r2 = new Node(1);
-		  Node r3 = new Node(2);
-		  Node r4 = new Node(4);
-		  Node r5 = new Node(12);
-		  Node r6 = new Node(25);
-		  Node r7 = new Node(10);
-		  Node r8 = new Node(14);
-		  Node r9 = new Node(23);
-		  Node r10 = new Node(52);
-		  Node r11 = new Node(7);
+public class CheckIfBinaryTreeIsBalanced {
 
-		  root.left = r2;
-		  root.right = r3;
-		  r2.left = r4;
-		  r2.right = r5;
-		  r3.left = r6;
-		  r3.right = r7;
-		  r4.right = r8;
-		  //r8.right = r10;
-		  r7.left = r9;
-		  //r9.left = r11;
-		  
-		  balanceBinaryTree(root);
-		  out.println(isBalanced);
-	}
+    static int balanceBinaryTree(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lHeight = balanceBinaryTree(root.left);
+        // Do not make unneccessary calles to right if left substree is not balanced
+        if (lHeight == -1) {
+            return -1;
+        }
+
+        int rHeight = balanceBinaryTree(root.right);
+
+        if (rHeight == -1 || Math.abs(lHeight - rHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(lHeight, rHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(3);
+        Node r2 = new Node(1);
+        Node r3 = new Node(2);
+        Node r4 = new Node(4);
+        Node r5 = new Node(12);
+        Node r6 = new Node(25);
+        Node r7 = new Node(10);
+        Node r8 = new Node(14);
+        Node r9 = new Node(23);
+        Node r10 = new Node(52);
+        Node r11 = new Node(7);
+
+        root.left = r2;
+        root.right = r3;
+        r2.left = r4;
+        r2.right = r5;
+        r3.left = r6;
+        r3.right = r7;
+        r4.right = r8;
+        //r8.right = r10;
+        r7.left = r9;
+        //r9.left = r11;
+
+        out.println(balanceBinaryTree(root) < 0 ? "Not balanaced!!" : "Balanced!!");
+    }
 }
