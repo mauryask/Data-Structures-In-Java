a56fed8d896fee3582b9c553924d130561bc957b
mauryask <mauryask1732@gmail.com>
2025-02-22 13:11:51 +0530

    Updated

--- Tree/CheckIfTwoNodesAreCousins.java
+++ Tree/CheckIfTwoNodesAreCousins.java
@@ -2,88 +2,117 @@
 ** https://www.geeksforgeeks.org/check-two-nodes-cousins-binary-tree/
 ** T(n) : O(n) 
 ** S(n) : O(n)
-*/
-
-import static java.lang.System.*;
+ */
 import java.util.*;
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	Node(int data)
-	{
-		this.data = data;
-	   right = left = null;
-	}
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        right = left = null;
+    }
 }
 
-public class CheckIfTwoNodesAreCousins
-{
-	static boolean isCousin(Node root, int a, int b)
-	{		
-		Queue<Node> q = new ArrayDeque<>();
-		q.add(root);
+public class CheckIfTwoNodesAreCousins {
 
-		while(!q.isEmpty())
-		{	            
-			root = q.poll();		     
-			boolean aFound = false;
-		    boolean bFound = false;
-			
-	   	    if(root.left != null)
-			{
-			   if(root.left.data == a)
-				   aFound = true;			   
-			   else if(root.left.data == b)
-				   bFound = true;
-			   
-			   q.add(root.left);
-			}
-			if(root.right != null)
-			{
-			   if(root.right.data == a)
-					aFound = true;
-			   else if(root.right.data == b)
-				   bFound = true;
-			
-			   q.add(root.right);
-			}	
+    //DFS aproach
+    static class NodeInfo {
 
-			if(aFound && bFound)
-				return true;			
-		}
-				
+        int level;
+        int parent;
+
+        NodeInfo(int level, int parent) {
+            this.level = level;
+            this.parent = parent;
+        }
+    }
+
+    static boolean aFound = false;
+    static boolean bFound = false;
+
+    void findParent(Node root, int parent, Map<Integer, NodeInfo> map, int level, int a, int b) {
+        if (root == null) {
+            return;
+        }
+
+        map.put(root.data, new NodeInfo(level, parent));
+
+        if (root.data == a) {
+            aFound = true;
+        } else if (root.data == b) {
+            bFound = true;
+        }
+
+        if (aFound && bFound) {
+            return;
+        }
+
+        findParent(root.left, root.data, map, level + 1, a, b);
+        findParent(root.right, root.data, map, level + 1, a, b);
+    }
+
+    //BFS Apperoach (Best approach)
+    static boolean isCousinUtil(Node root, int a, int b) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            // As the both nodes going to be at same level
+            // Good idea to declare variables here only
+            int aParent = -1, bParent = -1;
+            boolean aFound = false, bFound = false;
+            int count = q.size();
+
+            while (count-- > 0) {
+                root = q.poll();
+
+                if (root.left != null) {
+                    if (root.left.data == a) {
+                        aParent = root.data;
+                        aFound = true;
+                    } else if (root.left.data == b) {
+                        bFound = true;
+                        bParent = root.data;
+                    }
+                    
+					//You need to push it even if some node is found
+                    q.add(root.left);
+                }
+
+                if (root.right != null) {
+                    if (root.right.data == a) {
+                        aParent = root.data;
+                        aFound = true;
+                    } else if (root.right.data == b) {
+                        bFound = true;
+                        bParent = root.data;
+                    }
+                    
+					//You need to push it even if some node is found to get another node
+                    q.add(root.right);
+                }
+
+                if (aFound && bFound && aParent != bParent) {
+                    return true;
+                }
+            }
+        }
+
         return false;
-	}
-	
-	public static void main(String[] args)
-	{
-		  Node root = new Node(20);
-		  Node r2 = new Node(8);
-		  Node r3 = new Node(22);
-		  Node r4 = new Node(4);
-		  Node r5 = new Node(12);
-		  Node r6 = new Node(25);
-		  Node r7 = new Node(10);
-		  Node r8 = new Node(14);
-		  Node r9 = new Node(23);
-		  Node r10 = new Node(52);
-		  Node r11 = new Node(7);
+    }
 
-		  root.left = r2;
-		  root.right = r3;
-		  r2.left = r4;
-		  r2.right = r5;
-		  r3.left = r6;
-		  r3.right = r7;
-		  r4.right = r8;
-		  r8.right = r10;
-		  r7.left = r9;
-		  r9.left = r11;
-          
-		  out.println(isCousin(root, 22,8));
- 	}
+    public boolean isCousins(Node root, int a, int b) {
+        Map<Integer, NodeInfo> map = new HashMap<>();
+        aFound = false;
+        bFound = false;
+        findParent(root, -1, map, 0, a, b);
+
+        return map.containsKey(a) && map.containsKey(b)
+                && map.get(a).parent != map.get(b).parent
+                && map.get(a).level == map.get(b).level;
+    }
 }
