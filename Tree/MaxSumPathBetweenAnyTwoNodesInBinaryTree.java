f4baccb887b92152d369569badc602e33201e02e
mauryask <mauryask1732@gmail.com>
2025-02-24 16:48:00 +0530

    Updated

--- Tree/MaxSumPathBetweenAnyTwoNodesInBinaryTree.java
+++ Tree/MaxSumPathBetweenAnyTwoNodesInBinaryTree.java
@@ -1,42 +1,47 @@
 /*
 * T(n) : O(n) 
 * S(n) : O(n) 
-*/
+ */
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int data)
-	{
-		this.data = data;
-		left = null;
-		right = null;
-	}
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
 
 public class MaxSumPathBetweenAnyTwoNodesInBinaryTree {
+
     static int max = Integer.MIN_VALUE;
 
-    static int solve(Node root){
-        if(root == null) return 0;
+    static int solve(Node root) {
+        if (root == null) {
+            return 0;
+        }
 
         int left = solve(root.left);
         int right = solve(root.right);
         //Sum of nodes between left subtree and right subtree 
         int sum = left + right + root.data;
-		// Max sum of left or right subtree nodes
+        // Max sum of left or right subtree nodes
         int sum1 = Math.max(left, right) + root.data;
-		// Calculate overall max value (Remember you have to include the root node as well in calculating max)
-		// As all the paths might be less as compare to root value 
+        // Calculate overall max value (Remember you have to include the root node as well in calculating max)
+        // As all the paths might be less as compare to root value 
         max = Math.max(root.data, Math.max(max, Math.max(sum, sum1)));
         //Retunrn max from left or right subtree (which ever is max)
         return Math.max(root.data, sum1);
     }
 
     public int maxPathSum(Node root) {
+        if (root == null) {
+            return 0;
+        }
         max = Integer.MIN_VALUE;
         solve(root);
         return max;
--- Tree/MaxSumPathBetweenTwoLeafNodesInBinaryTree.java
+++ Tree/MaxSumPathBetweenTwoLeafNodesInBinaryTree.java
@@ -1,76 +1,42 @@
 /*
 * T(n) : O(n) 
 * S(n) : O(n) 
-*/
-import static java.lang.System.*;
-import java.util.*;
+ */
+public class MaxSumPathBetweenTwoLeafNodesInBinaryTree {
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int data)
-	{
-		this.data = data;
-		left = null;
-		right = null;
-	}
-}
+    static int maxSum = Integer.MIN_VALUE;
 
-public class MaxSumPathBetweenTwoLeafNodesInBinaryTree
-{
-	static int sum = 0;
-	static int max1 = 0;
-	static int max2 = 0;
-	
-	static void calcMax(Node root)
-	{
-		if(root != null)
-		{
-			sum += root.data;
-			
-			calcMax(root.left);
-			calcMax(root.right);
-			
-			if(root.left == null && root.right == null)
-			{
-				if(sum > max1)
-				{
-					max2 = Math.max(max1, max2);
-					max1 = Math.max(max1, sum);
-				}
-				else
-				{
-					max2 = Math.max(max2, sum);
-				}
-			}
-			
-			sum -= root.data;
-		}
-	}
-	
-	  public static void main(String [] args)
-		{		
-			  Node root = new Node(1);
-			  Node r2 = new Node(2);
-			  Node r3 = new Node(3);
-			  Node r4 = new Node(4);
-			  Node r5 = new Node(5);
-			  Node r6 = new Node(12);
-			  Node r7 = new Node(-7);
-			  Node r8 = new Node(8);
-			  
-			  root.left = r2;
-			  root.right = r3;
-			  r2.left = r4;
-			  r2.right = r5;
-			  r3.left = r6;
-			  r3.right = r7;
-			  r4.right = r8;
-			  
-			  calcMax(root);			  
-			  out.println(max1 + max2);
-		}
+    int findMaxLeafSum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        // If it is lead node return data directly
+        if (root.left == null && root.right == null) {
+            return root.data;
+        }
+
+        int leftSum = findMaxLeafSum(root.left);
+        int rightSum = findMaxLeafSum(root.right);
+        
+        // If a non leaf node encountered with left and rigth subtrees as non null
+		// Get the max sum between leaf nodes
+        if (root.left != null && root.right != null) {
+            maxSum = Math.max(maxSum, leftSum + rightSum + root.data);
+            return Math.max(leftSum, rightSum) + root.data;
+        }
+       
+	    // If left subtree is not there return max right sum
+        if (root.left == null) {
+            return rightSum + root.data;
+        } else { //Else return max left sum
+            return leftSum + root.data;
+        }
+    }
+
+    int maxPathSum(Node root) {		
+        maxSum = Integer.MIN_VALUE;
+        findMaxLeafSum(root);
+        return maxSum;
+    }
 }
