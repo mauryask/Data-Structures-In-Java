c2206588042b2f76bec515cf8005f37d14222df7
mauryask <mauryask1732@gmail.com>
2025-02-24 21:30:38 +0530

    Updated

--- Tree/MinimumTurnsNeededToReachFromOneNodeToAnother.java
+++ Tree/MinimumTurnsNeededToReachFromOneNodeToAnother.java
@@ -2,138 +2,93 @@
 * T(n) : O(n) 
 * S(n) : O(1)
 ** https://www.geeksforgeeks.org/number-turns-reach-one-node-binary-tree/
-*/
+ */
 
 import static java.lang.System.*;
 import java.util.*;
 
-class Node 
-{
-	int data;
-	Node left;
-	Node right;
-	
-	Node(int data)
-	{
-	   this.data = data;
-	   left = right = null;
-	}
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
 }
 
-public class MinimumTurnsNeededToReachFromOneNodeToAnother 
-{
-    static String prev = "X";
-	static int count = 0;
-    
-    static Node getLca(Node root, int first, int second)
-    {
-        if(root == null)
-          return null;
-        
-        if(root.data == first || root.data == second)
-           return root;
-           
-        Node left = getLca(root.left, first, second);
-        Node right =  getLca(root.right, first, second);
-        
-        if(left != null && right != null)
-          return root;
-        
-        return left == null ? right : left;  
+public class MinimumTurnsNeededToReachFromOneNodeToAnother {
+    // Get the LCA
+    static Node lca(Node root, int m, int n) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.data == m || root.data == n) {
+            return root;
+        }
+
+        Node left = lca(root.left, m, n);
+        Node right = lca(root.right, m, n);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left == null ? right : left;
     }
-        
-    static boolean turnCount(Node root, int value)
-    {
-        if(root == null)
-          return false;
-          
-        if(root.data == value)
-            return true;
-            
-        boolean left = turnCount(root.left, value);
-        
-        if(left)
-        {
-            if(!prev.equals("L"))
-            {
-                prev = "L";
-                count++;
-            }
-            
+
+    static int turnCount = 0;
+    static int prev = -1;
+    // 0 : left turn
+    // 1 : right turn
+
+    static boolean nodeToRootPath(Node root, int value) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == value) {
             return true;
         }
-        else
-        {
-            boolean right = turnCount(root.right, value);
-            
-            if(right)
-            {
-                if(!prev.equals("R"))
-                {
-                    prev = "R";
-                    count++;
-                }
+
+        boolean left = nodeToRootPath(root.left, value);
+
+        if (left) {
+            // If prev turn is not 0 it means a turn taken
+            if (prev != 0) {
+                turnCount++;
             }
-            
-            return right;
+            prev = 0;
+            return true;
         }
-        
+
+        boolean right = nodeToRootPath(root.right, value);
+
+        if (right) {
+            // If prev turn is not 1 it means a turn taken
+            if (prev != 1) {
+                turnCount++;
+            }
+            prev = 1;
+            return true;
+        }
+
+        return false;
     }
-    
-    static int NumberOfTurns(Node root, int first, int second)
-    {
-        Node lca = getLca(root, first, second);
-		
-		out.println(lca.data);
-		
-        turnCount(lca, first);
-        int count1 = count;
-		
-		out.println(count1);
-		
-        count = 0;
-        prev = "X";
-		
-        turnCount(lca, second);
-		
-		out.println(count);
-		
-        return count1 + count - 1;
+
+    static int numberOfTurns(Node root, int first, int second) {        
+        if(root == null) return -1;
+        Node lcaNode = lca(root, first, second);        
+        if(lcaNode == null) return -1;
+        turnCount = 0;
+        prev = -1;
+        nodeToRootPath(lcaNode, first);
+        //Reset prev turn for another value
+        prev = -1;
+        nodeToRootPath(lcaNode, second);
+        return turnCount - 1 == 0 ? -1 : turnCount - 1;
     }
-	
-	public static void main(String [] args)
-	{
-		  Node root = new Node(1);
-		  Node r2 = new Node(2);
-		  Node r3 = new Node(3);
-		  Node r4 = new Node(4);
-		  Node r5 = new Node(5);
-		  Node r6 = new Node(6);
-		  Node r7 = new Node(7);
-		  Node r8 = new Node(8);
-		  
-		  Node r9 = new Node(9);
-		  Node r10 = new Node(10);
-		  Node r11 = new Node(11);
-		  Node r12 = new Node(12);
-		  
-		  
-		  root.left = r2;
-		  root.right = r3;
-		  r2.left = r4;
-		  r2.right = r5;
-		  r3.left = r6;
-		  r3.right = r7;
-		  r7.right = r8;
-		  
-		  r4.left = r9;
-		  r4.right = r10;
-		  r5.right = r11;
-		  r7.left = r12;
-		  
-		  int src = 11;
-		  int dest = 5;
-		  
-		  out.println(NumberOfTurns(root, src, dest));		  
-	}
 }
