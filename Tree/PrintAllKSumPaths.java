69deda956f5b2ecd44dd543055ac447296bf6f24
mauryask <mauryask1732@gmail.com>
2025-02-24 23:08:34 +0530

    Updated

--- Tree/PrintAllKSumPaths.java
+++ Tree/PrintAllKSumPaths.java
@@ -1,77 +1,89 @@
-/**
-Time complexity  : O(n)
-Space complexity : O(n)
-** Here are printing root to leaf 
-** paths
-*/
 
+/**
+ * Time complexity  : O(n)
+ * Space complexity : O(n)
+ ** Here are printing root to leaf
+ ** paths
+ */
 import static java.lang.System.*;
 import java.util.*;
 
-class Node
-{
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int data)
-	{
-		this.data = data;
-		left = null;
-		right = null;
-	}
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
 
-public class PrintAllKSumPaths
-{
-   static  int sum = 0;
-   static Stack<Integer> stack = new Stack<>();
-	
-   static void kSumPath(Node root, int k)
-   {	   
-	   if(root == null)
-		   return;
-	   
-	   sum += root.data;
-	   stack.push(root.data);
-	   
-	   if(sum == k)
-		   printPath(stack);
-	   
-	   kSumPath(root.left, k);
-	   kSumPath(root.right, k);
-	   
-	   sum -= root.data;
-	   stack.pop();	   
-   }
-   
-   
-   static void printPath(Stack<Integer> stack)
-   {
-	   for(int x : stack)
-		   out.print(x+" ");
-   }
-   
-	  public static void main(String [] args)
-		{		
-			  Node root = new Node(1);
-			  Node r2 = new Node(2);
-			  Node r3 = new Node(3);
-			  Node r4 = new Node(4);
-			  Node r5 = new Node(5);
-			  Node r6 = new Node(6);
-			  Node r7 = new Node(7);
-			  Node r8 = new Node(8);
-			  
-			  root.left = r2;
-			  root.right = r3;
-			  r2.left = r4;
-			  r2.right = r5;
-			  r3.left = r6;
-			  r3.right = r7;
-			  r4.right = r8;
-			 
-			 
-			 kSumPath(root, 7);
-		}
+public class PrintAllKSumPaths {
+
+    static void kSumPath(Node root, List<Integer> list, int k) {
+        if (root == null) {
+            return;
+        }
+
+        list.add(root.data);
+        countPath(list, k);
+
+        kSumPath(root.left, list, k);
+        kSumPath(root.right, list, k);
+
+        list.remove(list.size() - 1);
+    }
+
+     //Count the number of paths
+    static void countPath(List<Integer> list, int k) {
+        int sum = 0;
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sum += list.get(i);
+            if (sum == k) {
+                count++;
+                printPath(i, list);
+            }
+        }
+    }
+     
+	//Print the path here
+    static void printPath(int startIndex, List<Integer> list) {
+        for (int i = startIndex; i < list.size(); i++) {
+            out.print(list.get(i) + " ");
+        }
+		out.println();
+    }
+
+    static int count = 0;
+
+    static int sumK(Node root, int k) {
+        count = 0;
+        kSumPath(root, new ArrayList<>(), k);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node r2 = new Node(2);
+        Node r3 = new Node(3);
+        Node r4 = new Node(4);
+        Node r5 = new Node(5);
+        Node r6 = new Node(6);
+        Node r7 = new Node(7);
+        Node r8 = new Node(8);
+
+        root.left = r2;
+        root.right = r3;
+        r2.left = r4;
+        r2.right = r5;
+        r3.left = r6;
+        r3.right = r7;
+        r4.right = r8;
+ 
+        sumK(root, 7);
+    }
 }
