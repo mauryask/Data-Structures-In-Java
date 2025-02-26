c8ff74c1e36c44dc5e9e81ad98c5cefe3b3d2b07
mauryask <mauryask1732@gmail.com>
2025-02-26 17:12:07 +0530

    Updated

--- Stack/InfixToPostfixConversion.java
+++ Stack/InfixToPostfixConversion.java
@@ -1,126 +1,60 @@
-// infix to post-fix conversion
-//https://www.geeksforgeeks.org/stack-set-2-infix-to-postfix/
-// watch "saurabh shukla" infix to post-fix video for algorithm
-/*
- * https://www.geeksforgeeks.org/convert-infix-prefix-notation/
- * This link shows how to convert infix to prefix
- * see the algorithm given in the website
- * */
-package com.pnstech;
+
 import java.util.*;
+
 public class InfixToPostfixConversion {
+    static int getOperatorPrecedence(char operator) {
+        switch (operator) {
+            case '+', '-' -> {
+                return 1;
+            }
+            case '*', '/', '%' -> {
+                return 2;
+            }
+            case '^' -> {
+                return 3;
+            }
+        }
 
-	static int precedence(char ch)
-	{
-		switch(ch)
-		{
-		case '+':
-		case '-':
-			return 1;
-			
-		case '*':
-		case '/':
-		case '%':
-			return 2;
-			
-		case '^':
-			return 3;
-		}
-		
-		return -1; 
-	}
-	
-	static String convertInfixToPostfix(String str)
-	{
-		Stack<Character> st = new Stack<>();
-		String rslt = new String("");
-		
-		
-		for(int i=0; i<str.length(); ++i)
-		{
-			char c = str.charAt(i);
-			
-			if(Character.isLetterOrDigit(c))
-				rslt = rslt + c;
-			
-			else if(c == '(')
-			   st.push(c);	
-			
-			
-			else if(c == ')')
-			{
-				while( !st.isEmpty() && st.peek() != '(')
-					rslt = rslt + st.pop();
-				
-				   st.pop();
-			}
-			
-			else
-			{
-				while(!st.isEmpty() && precedence(c) <= precedence(st.peek()))
-				{
-					if(st.peek() == '(')
-						return "Invalid Expression";
-					rslt = rslt + st.pop();
-				}
-				
-				st.push(c);
-			}
-			
-				
-		}
-		
-		while(!st.isEmpty())
-		{
-			if(st.peek() == '(')
-				return "Invalid Expression";
-			rslt = rslt + st.pop();	
-		}
-		
-		return rslt;
-		
-	}
-	
-	public static void main(String[] args) {
-	
-		
-		/***
-		 * Infix to post-fix conversion
-		 * */
-		
-		
-		Scanner sc = new Scanner(System.in);
-	    String str = sc.next(); //enter infix expression
+        return -1;
+    }
 
-	    System.out.println(convertInfixToPostfix(str));
-	    
-	   
-	    
-	    /***
-	     * Infix to prefix conversion
-	     * https://www.geeksforgeeks.org/convert-infix-prefix-notation/
-	     * see algorithm from the given link
-	     */
-	    
-	/*    StringBuilder strx  = new StringBuilder("(a-b/c)*(a/k-l)"); //infix expression
-	    strx.reverse();
-	    char ch [] = strx.toString().toCharArray();
-	    //reverse this string
+    public static String infixToPostfix(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-	    System.out.println(strx); //Output: )l-k/a(*)c/b-a(
-	    
-	    for(int i=0; i<ch.length; i++)
-	    {
-	    	if(ch[i] == ')')
-	    		ch[i] = '(';
-	    	else if(ch[i] == '(')
-	    		ch[i] = ')';
-	    }
-	    
-	    strx = new StringBuilder(convertInfixToPostfix(new String(ch)));
-	    System.out.print(strx.reverse());
- 		*/
-	    sc.close();	
-	}
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(ch);
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+
+                stack.pop();
+            } else {
+                while (!stack.isEmpty()
+                        && (getOperatorPrecedence(ch) <= getOperatorPrecedence(stack.peek()))) {
+                    sb.append(stack.pop());
+                }
+
+                stack.push(ch);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String infixExpression = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println(infixToPostfix(infixExpression));
+    }
 }
